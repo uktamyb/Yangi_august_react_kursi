8fc57b50087dc33c8acc33c91abf829075667188
UktamHudoyaorv <uktamyb@gmail.com>
2021-08-03 23:52:38 +0500

    React darslari boshlandi

---
+++ src/Student.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class Student extends React.Component {
+  render() {
+    return <div>Hello from Students</div>;
+  }
+}
+export default Student;
+// export {Student, Students} // bir nechta class larni yuozish mumkin.
+
+// function Test(){
+
+// }
+
+// Test();
