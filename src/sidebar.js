f0503d5fa3ad5051d3d812d456c329a7c359d515
UktamHudoyaorv <uktamyb@gmail.com>
2021-08-05 19:06:11 +0500

    change

--- src/sidebar.js
+++ src/sidebar.js
@@ -10,9 +10,17 @@ export default class sidebar extends Component {
         </div>
         <div className="menu">
           <h5>Menu</h5>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Garage</a></li>
+            <li><a href="#">Service Menu</a></li>
+            <li><a href="#">Racers</a></li>
+            <li><a href="#">Calculator</a></li>
+            <li><a href="#">Settings</a></li>
+          </ul>
         </div>
         <div className="driver">
-          `<h5>Sheduled Races</h5>
+          <h5>Sheduled Races</h5>
         </div>
       </div>
     )
