b486302fc128ba0991e84fd66d7ed19db7984742
UktamHudoyaorv <uktamyb@gmail.com>
2021-08-07 01:14:41 +0500

    new changes

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './navbar.css'
+import './navbar.css';
+import SearchIcon from './img/search.png';
 
 export default class navbar extends Component {
   render() {
     return (
       <div className="nav-container">
-        Navbar
+        <div class="search">
+          <img src={SearchIcon} alt="search" className="search-icon" />
+          <input placeholder="    Search for a race, car or a driver" />
+        </div>
       </div>
     )
   }
